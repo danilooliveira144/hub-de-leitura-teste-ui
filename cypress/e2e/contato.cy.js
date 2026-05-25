8f6a991f28aa8399577b03ef990bab10de8a8916
Danilo Oliveira <danilooliveira4586@gmail.com>
2026-05-25 18:35:54 -0300

    Adicionando configurações de ambiente e os primeiros cenários da funcionalidade contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,46 @@
+describe('Funcionalidade: Contato', () => {
+  beforeEach(() => {
+    cy.visit('index.html');
+  });
+
+  it('Deve preencher o formulário de contato com sucesso', () => {
+    cy.get('#name').type('Danilo de Oliveira Marques');
+    cy.get('#email').type('danilo.oliveira@example.com');
+    cy.get('#subject').select('Sugestões');
+    cy.get('#message').type('Mensagem de teste para o formulário de contato.');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain','Contato enviado com sucesso!');
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o campo Nome', () => {
+    cy.get('#email').type('danilo.oliveira@example.com');
+    cy.get('#subject').select('Sugestões');
+    cy.get('#message').type('Mensagem de teste para o formulário de contato.');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain','Por favor, preencha o campo Nome.');
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o campo Email', () => {
+    cy.get('#name').type('Danilo de Oliveira Marques');
+    cy.get('#subject').select('Sugestões');
+    cy.get('#message').type('Mensagem de teste para o formulário de contato.');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain','Por favor, preencha o campo E-mail.');
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o campo Assunto', () => {
+    cy.get('#name').type('Danilo de Oliveira Marques');
+    cy.get('#email').type('danilo.oliveira@example.com');
+    cy.get('#message').type('Mensagem de teste para o formulário de contato.');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain','Por favor, selecione o Assunto.');
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o campo Mensagem', () => {
+    cy.get('#name').type('Danilo de Oliveira Marques');
+    cy.get('#email').type('danilo.oliveira@example.com');
+    cy.get('#subject').select('Sugestões');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain','Por favor, escreva sua Mensagem.');
+  });
+});
